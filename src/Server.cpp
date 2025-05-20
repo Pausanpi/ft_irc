dd3f76b8afc017f608839f5aed88e4e2a542dcbe
Paula <pausanpi1606@gmail.com>
2025-05-20 15:10:06 +0200

    Implement message sending restriction to channel members only

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: pausanch <pausanch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 17:41:00 by pausanch          #+#    #+#             */
-/*   Updated: 2025/05/20 13:01:08 by pausanch         ###   ########.fr       */
+/*   Updated: 2025/05/20 13:23:33 by pausanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -151,24 +151,66 @@ void Server::handleInput(Client &client, const std::string &input) {
             << " :Welcome to ft_irc, " << client.getNickname() << "!\r\n";
         client.sendMessage(oss.str());
     } else if (command == "PRIVMSG") {
-        std::string target;
-        iss >> target;
-        std::string msg;
-        std::getline(iss, msg);
-        if (msg.size() >= 2 && msg[1] == ':')
-            msg = msg.substr(2);
-        else if (msg[0] == ' ')
-            msg = msg.substr(1);
+		std::string target;
+		iss >> target;
 
-        for (int i = 0; i < MAX_CLIENTS; ++i) {
-            if (_clients[i].getFd() > 0 && _clients[i].getNickname() == target) {
-                std::ostringstream oss;
-                oss << ":" << client.getNickname()
-                    << " PRIVMSG " << target << " :" << msg << "\r\n";
-                _clients[i].sendMessage(oss.str());
-                break;
-            }
-        }
+		std::string msg;
+		std::getline(iss, msg);
+
+		// Remove leading spaces
+		size_t pos = msg.find_first_not_of(' ');
+		if (pos != std::string::npos) {
+			msg = msg.substr(pos);
+		} else {
+			msg.clear(); // no message content
+		}
+
+		// Remove leading ':' if present (common in IRC protocol)
+		if (!msg.empty() && msg[0] == ':') {
+			msg = msg.substr(1);
+		}
+
+		std::ostringstream oss;
+		oss << ":" << client.getNickname()
+			<< " PRIVMSG " << target << " :" << msg << "\r\n";
+		std::string fullMsg = oss.str();
+
+		if (!target.empty() && target[0] == '#') {
+			// Message to channel
+			std::map<std::string, Channel>::iterator it = _channels.find(target);
+			if (it != _channels.end()) {
+				Channel &channel = it->second;
+				const std::set<Client *> &members = channel.getMembers();
+
+				// Check if sender is in the channel members
+				if (members.find(&client) == members.end()) {
+					// Client is NOT in the channel, reject sending message
+					// Optionally send an error message back to client:
+					client.sendMessage("404 " + client.getNickname() + " " + target + " :Cannot send to channel (not a member)\r\n");
+					return;
+				}
+
+				// Client is in the channel, send the message to all other members
+				for (std::set<Client *>::const_iterator iter = members.begin(); iter != members.end(); ++iter) {
+					Client *member = *iter;
+					if (member->getFd() != client.getFd()) {
+						member->sendMessage(fullMsg);
+					}
+				}
+			} else {
+				// Channel not found: optionally handle error
+				client.sendMessage("403 " + client.getNickname() + " " + target + " :No such channel\r\n");
+			}
+		} else {
+			// Message to user
+			for (int i = 0; i < MAX_CLIENTS; ++i) {
+				if (_clients[i].getFd() > 0 && _clients[i].getNickname() == target) {
+					_clients[i].sendMessage(fullMsg);
+					break;
+				}
+			}
+		}
+
 	} else if (command == "JOIN") {
         std::string chanName;
         iss >> chanName;
